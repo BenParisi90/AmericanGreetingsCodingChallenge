104edbd48b1e50240f602b630b802e651f92f2e0
ParisiDesign <ben@parisidesign.com>
2021-01-11 00:17:44 -0800

    coding challenge first pass

---
+++ AmericanGreetings_UnityProject/Assets/Scripts/AnimationEvents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationEvents : MonoBehaviour
+{
+    Sprite nextSprite;
+    public Image image;
+    public AudioSource pageTurnSound;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetNextSprite(Sprite newSprite)
+    {
+        nextSprite = newSprite;
+        animator.SetTrigger("Swap");
+        pageTurnSound.Play();
+    }
+
+    public void SwapSprite()
+    {
+        image.sprite = nextSprite;
+    }
+}
---
+++ AmericanGreetings_UnityProject/Assets/Scripts/ShapeLogic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShapeLogic : MonoBehaviour, IPointerDownHandler
+{
+    public AudioSource colorChangeSound;
+    public Image colorChangeImage;
+    float lastClickTime = -999;
+    float doubleClickTime = .5f;
+    Image image;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.alphaHitTestMinimumThreshold = .5f;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if(Time.time - lastClickTime < doubleClickTime)
+        {
+            lastClickTime = -999;
+            Color newColor = new Color(
+                Random.Range(0f, 1f), 
+                Random.Range(0f, 1f), 
+                Random.Range(0f, 1f)
+            );
+            colorChangeImage.color = newColor;
+            colorChangeSound.Play();
+        }
+        else
+        {
+            lastClickTime = Time.time;
+        }
+    }
+}
